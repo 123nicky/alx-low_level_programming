a40343dbc616d5715db321af5462fa99784c2e41
123nicky <nicolekaswa@gmail.com>
2022-07-03 09:21:13 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main -Prints the alphabet in reverse.
+*
+* Return: Always 0.
+*/
+int main(void)
+{int d = 'z';
+while (d >= 'a')
+{
+putchar(d)
+d--;
+}
+putchar('\n');
+return (0);
+}
