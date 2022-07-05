8d8e115d7cc822ffa8cdc87458f9aa9c6286798d
123nicky <nicolekaswa@gmail.com>
2022-07-05 12:20:40 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/*
+* File: 1-alphabet.c
+*
+* Auth: 123nicky
+*/
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+*/
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 97; alpha <= 122; alpha++)
+_putchar(alpha);
+_putchar('\n');
+}
